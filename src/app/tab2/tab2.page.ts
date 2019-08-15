3f185681ceed39d52bab79ea153a1ab68904e633
AlexandreLac <alexandrelac248@gmail.com>
2019-08-15 17:46:58 +0200

    Test form ionic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+	gameplayKey: string;
+  	user:Object;
+	
+	/*  constructor(private router: Router, private alertController: AlertController,private http: HTTP)
+ 		{}
 
-  constructor() {}
+	/*onSubmit(form)
+	{
+
+		console.log("Form value : ",form.form.value);
+		/*this.http.patch('https://o-killer.herokuapp.com/user/'+this.gameplayKey,form.form.value,{}).then(data => {
+			/*this.editForm();
+			this.router.navigateByUrl('');*/
+			//console.log(data);
+		//})
+		
+		
+	//}
+
+
+	/*async editForm()
+	{
+		const edit = await this.alertController.create({
+			header: 'User updated',
+			message: 'Your are join the party',
+			buttons:[
+			{
+				text: 'OK',
+				handler: () => { }
+			}
+			]
+		});
+		await edit.present();
+	}*/
 
 }
