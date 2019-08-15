05541a3685983ff55f585f10722216db36c55c49
Alan <m.limaalan@hotmail.fr>
2019-08-16 00:20:48 +0200

    add All Service

---
+++ src/app/service/question.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+  url = 'https://o-killer.herokuapp.com/';
+
+  constructor(private http: HttpClient) { }
+}
