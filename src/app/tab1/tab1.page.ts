6d2cd450c31ebabbf19f7561c5385d3aec62cc1b
Alan <m.limaalan@hotmail.fr>
2019-08-16 18:16:08 +0200

    design

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  gameplay: any = null; 
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+
+  }
+
+  newGame() {
+  }
+
+  joinParty() {
+  }
+
+  logIn() {
+  }
+
+  /**
+   * The goal here is to pass information from a page to another
+   * Start point
+   */
+  // test() {
+  //   let navigationExtras: NavigationExtras = {
+  //     state: {
+  //       gameplay: this.gameplay
+  //     }
+  //   };
+  //   this.router.navigate(['where'], navigationExtras);
+  // }
+
+  /**
+   *    The Other side
+   */
+  // data: any;
+ 
+  // constructor(private route: ActivatedRoute, private router: Router) {
+  //   this.route.queryParams.subscribe(params => {
+  //     if (this.router.getCurrentNavigation().extras.state) {
+  //       this.data = this.router.getCurrentNavigation().extras.state.user;
+  //     }
+  //   });
+  // }
 }
