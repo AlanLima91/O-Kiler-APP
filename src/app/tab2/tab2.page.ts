0a7dbe2bec10001ebecf6abb9a72ee88ed52f14b
Alan <m.limaalan@hotmail.fr>
2019-08-16 16:35:16 +0200

    refactor tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-tab2',
@@ -12,40 +12,29 @@ export class Tab2Page {
 	gameplayKey: string;
   	user:Object;
 	
-	constructor(private route: ActivatedRoute,private router: Router, private alertController: AlertController, private http: HttpClient){}
+	constructor(private route: ActivatedRoute,private router: Router, private alertController: AlertController, private userService: UserService){}
 
 	onSubmit(form)
-	{
-		const httpOptions = {
-			headers: new HttpHeaders({
-				'Content-Type':  'application/json',
-				'Accept':  'application/json'
-			})
-		};
-		
+	{	
 		console.log("Form value : ",form.form.value);
-		this.http.patch('https://o-killer.herokuapp.com/user/5d54a0d24c99c61a8811d32f',form.form.value,httpOptions).subscribe(data => {
+
+		this.userService.updateUser('5d54a0d24c99c61a8811d32f', form.form.value).subscribe(data => {
 			this.editForm();
-			//this.router.navigateByUrl('');
-			console.log(data);
 		})
-		
-		
 	}
 
 	async editForm()
 	{
 		const edit = await this.alertController.create({
 			header: 'User updated',
-			message: 'Your are join the party',
+			message: 'Your have join the party',
 			buttons:[
-			{
-				text: 'OK',
-				handler: () => { }
-			}
+				{
+					text: 'OK',
+					handler: () => { }
+				}
 			]
 		});
 		await edit.present();
 	}
-
 }
