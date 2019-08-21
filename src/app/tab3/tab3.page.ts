162adfb5442bee925a219fd141cb7782652012a6
Alan <m.limaalan@hotmail.fr>
2019-08-21 12:06:02 +0200

    submit on tab 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from '../class/user';
+import { UserService } from '../service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  // On essaye de recevoir l'id du gameplay pour avoir accès à la liste des utilisateurs.
+  constructor(private router: Router, private userService: UserService) {}
 
-  constructor() {}
+  onSubmitNew(form) {
+    console.log('new submit');
+    let user: User = new User(null, form.form.value.username, null, form.form.value.password);
 
+    this.userService.updateUser('null', user).subscribe(data => { // Replace le null par l'id de l'user
+      console.log(data);
+    });
+  }
 }
