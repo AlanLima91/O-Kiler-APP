84bb7bdc937101433b3264795c9b67b75b6b167b
Alan <m.limaalan@hotmail.fr>
2019-08-21 12:06:36 +0200

    add gamers to class.gameplay

--- src/app/class/gameplay.ts
+++ src/app/class/gameplay.ts
@@ -5,13 +5,15 @@ export class Gameplay {
     public startTime    :any;
     public level        :number;
     public nbJoueur     :any;
+    public gamers       :any;
 
-    constructor(name: string, level: number, nbJoueur: any, _id: string = null,  duree: any = null, startTime: any = null) {
+    constructor(name: string, level: number, nbJoueur: any= null, _id: string = null,  duree: any = null, startTime: any = null, gamers: any = null) {
         this._id        = _id;
         this.name       = name;
         this.duree      = duree;
         this.startTime  = startTime;
         this.level      = level;
         this.nbJoueur   = nbJoueur;
+        this.gamers     = gamers;
     }
 }
