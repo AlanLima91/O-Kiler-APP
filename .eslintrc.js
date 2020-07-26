e101585f1ea8e54dc04769686a5b99ff573fd178
Alan <m.limalan@hotmail.fr>
2020-07-26 16:13:06 +0200

    add more config to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,36 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true
+    es6: true,
+    node: true
   },
   extends: [
-    'standard'
+    'standard',
+    'plugin:react/recommended'
   ],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
+  plugins: [
+    'react',
+    'react-native'
+  ],
   rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/sort-styles': [
+      'error',
+      'asc', {
+        ignoreClassNames: false,
+        ignoreStyleProperties: false
+      }]
   }
 }
